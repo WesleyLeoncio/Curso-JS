a84468a6ff70543a9256e3cbf602ee67d2ae79f7
Wesley Leoncio <wesley.leoncio.izi@gmail.com>
2020-12-27 16:25:10 -0300

    Atualização

--- gulp/spa/gulpTasks/servidor.js
+++ gulp/spa/gulpTasks/servidor.js
@@ -1,12 +1,26 @@
 const gulp = require('gulp');
+const webserver = require('gulp-webserver'); // responsavel por servir os dados
+const watch = require('gulp-watch'); // responsavel pro monitorar e atualizar os dados da aplicação
+
+function servidor() {
+    return gulp.src('build')
+        .pipe(webserver({
+            port: 8080,
+            open: true,
+            livereload: true
+
+        }))
+}
 
 function monitorarArquivo(cb) {
+    watch('src/**/*.html', () => gulp.series('appHTML')());
+    watch('src/**/*.scss', () => gulp.series('appCSS')());
+    watch('src/**/*.js', () => gulp.series('appJS')());
+    watch('src/assets/imgs/**/*.*', () => gulp.series('appIMG')());
     return cb();
 }
 
-function servidor(cb) {
-    return cb();
-}
+
 
 module.exports = {
     monitorarArquivo,

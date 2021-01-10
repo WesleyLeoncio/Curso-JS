cc381c1fcbbcbcc1673a84e1fef4bbd29d06ae7a
Wesley Leoncio <wesley.leoncio.izi@gmail.com>
2021-01-10 19:24:00 -0300

    Atualização

---
+++ react/exercicios/src/componentes/Primeiro.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Pagina de Teste </h1>
+        </div>
+    );
+}
+
+
+
+export default App;
+
